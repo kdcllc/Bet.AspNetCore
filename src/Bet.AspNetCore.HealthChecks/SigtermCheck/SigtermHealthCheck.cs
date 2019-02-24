015683f553ba2b879d6f133e415b9e9892fe6b6e
kdcllc <info@kingdavidconsulting.com>
2019-02-24 09:48:13 -0500

    add sigterm healchcheck

---
+++ src/Bet.AspNetCore.HealthChecks/SigtermCheck/SigtermHealthCheck.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Extensions.Diagnostics.HealthChecks;
+using Microsoft.Extensions.Hosting;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Bet.AspNetCore.HealthChecks.SigtermCheck
+{
+    public class SigtermHealthCheck : IHealthCheck
+    {
+        private readonly IApplicationLifetime _applicationLifetime;
+
+        public SigtermHealthCheck(IApplicationLifetime applicationLifetime)
+        {
+            _applicationLifetime = applicationLifetime;
+        }
+
+        public async Task<HealthCheckResult> CheckHealthAsync(
+            HealthCheckContext context,
+            CancellationToken cancellationToken = default)
+        {
+            if (_applicationLifetime.ApplicationStopping.IsCancellationRequested)
+            {
+                return new HealthCheckResult(
+                    context.Registration.FailureStatus,
+                    description: "IApplicationLifetime.ApplicationStopping was requested");
+            }
+
+            return HealthCheckResult.Healthy();
+        }
+    }
+}
