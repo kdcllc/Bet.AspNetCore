dddfb98d82c870aaffeb283c153b011d935cb87c
kdcllc <info@kingdavidconsulting.com>
2019-03-26 00:01:25 -0400

    some spam/ham ideas

---
+++ src/Bet.AspNetCore.Sample/Models/Message.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.ML.Data;
+
+namespace Bet.AspNetCore.Sample.Models
+{
+    internal class Message
+    {
+        //[LoadColumn(0)]
+        public string TextLabel { get; set; }
+
+        //[LoadColumn(1)]
+        public string Text { get; set; }
+
+        public bool Label { get; set; }
+    }
+}
