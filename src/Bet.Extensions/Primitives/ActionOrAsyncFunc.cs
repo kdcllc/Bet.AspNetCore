29ca03afd92cda9a2e9b59a1c7b4d58f4fca64f6
kdcllc <info@kingdavidconsulting.com>
2019-04-28 19:33:48 -0400

    add action or async fuc execution

---
+++ src/Bet.Extensions/Primitives/ActionOrAsyncFunc.cs
@@ -0,0 +1,38 @@
+﻿using System.Threading.Tasks;
+
+namespace System.Threading
+{
+    /// <summary>
+    /// Provides a way to create a executable task or section of code either way.
+    /// </summary>
+    public class ActionOrAsyncFunc
+    {
+        private readonly Action _action;
+        private readonly Func<Task> _asyncAction;
+        private readonly bool _isAsync;
+
+        public ActionOrAsyncFunc(Action action)
+        {
+            _isAsync = false;
+            _action = action;
+        }
+
+        public ActionOrAsyncFunc(Func<Task> asyncAction)
+        {
+            _isAsync = true;
+            _asyncAction = asyncAction;
+        }
+
+        public async Task Invoke()
+        {
+            if (_isAsync)
+            {
+                await _asyncAction();
+            }
+            else
+            {
+                _action();
+            }
+        }
+    }
+}
