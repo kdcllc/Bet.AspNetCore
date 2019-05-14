a5efed8f77257c037065d65bf6c9e2028eb426f3
kdcllc <info@kingdavidconsulting.com>
2019-05-14 09:41:39 -0400

    fix the bug issue #37

--- src/Bet.Extensions.AzureVault/AzureVaultKeyBuilder.cs
+++ src/Bet.Extensions.AzureVault/AzureVaultKeyBuilder.cs
@@ -6,6 +6,7 @@ using Bet.AspNetCore.Options;
 using Bet.Extensions.AzureVault;
 
 using Microsoft.Azure.KeyVault;
+using Microsoft.Azure.KeyVault.Models;
 using Microsoft.Azure.Services.AppAuthentication;
 using Microsoft.Extensions.Configuration.AzureKeyVault;
 
@@ -81,13 +82,16 @@ namespace Microsoft.Extensions.Configuration
 
                     return builder.Build();
                 }
-                catch (AzureServiceTokenProviderException)
+                catch (Exception)
                 {
                     var list = builder.Sources.ToList();
-                    var found = list.FirstOrDefault(x => x.GetType().FullName.Contains("AzureKeyVaultConfigurationSource"));
+                    var found = list.Where(x => x.GetType().FullName.Contains("AzureKeyVaultConfigurationSource"));
                     if (found != null)
                     {
-                        builder.Sources.Remove(found);
+                        foreach (var item in found)
+                        {
+                            builder.Sources.Remove(item);
+                        }
                     }
                 }
             }
