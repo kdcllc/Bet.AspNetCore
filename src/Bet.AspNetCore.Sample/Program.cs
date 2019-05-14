859fe238e079636119df93d5129a61b422ed961e
kdcllc <info@kingdavidconsulting.com>
2019-05-14 09:43:57 -0400

    update the project to ran but without ApiVersioning for now issue #36

--- src/Bet.AspNetCore.Sample/Program.cs
+++ src/Bet.AspNetCore.Sample/Program.cs
@@ -2,6 +2,7 @@ using System;
 using System.Diagnostics.CodeAnalysis;
 
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 
 namespace Bet.AspNetCore.Sample
@@ -21,6 +22,19 @@ namespace Bet.AspNetCore.Sample
                         {
                             webBuilder.UseShutdownTimeout(TimeSpan.FromSeconds(20));
                             webBuilder.UseStartup<Startup>();
+
+                            webBuilder.ConfigureAppConfiguration((hostingContext, configBuilder) =>
+                            {
+                                // based on environment Development = dev; Production = prod prefix in Azure Vault.
+                                var envName = hostingContext.HostingEnvironment.EnvironmentName;
+                                var configuration = configBuilder.AddAzureKeyVault(hostingEnviromentName: envName, usePrefix: true);
+
+                                // helpful to see what was retrieved from all of the configuration providers.
+                                if (hostingContext.HostingEnvironment.IsDevelopment())
+                                {
+                                    configuration.DebugConfigurations();
+                                }
+                            });
                         });
         }
     }
