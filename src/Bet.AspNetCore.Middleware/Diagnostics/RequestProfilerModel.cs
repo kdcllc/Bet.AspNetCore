7b8e1fad735d0d0d66564f7ead1b7755ee5f2a9c
KDCLLC <info@kingdavidconsulting.com>
2019-11-22 00:39:00 -0500

    adding the middleware and upgrading blob library issue #83

---
+++ src/Bet.AspNetCore.Middleware/Diagnostics/RequestProfilerModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+
+namespace Bet.AspNetCore.Middleware.Diagnostics
+{
+    /// <summary>
+    /// Request Response Profile model.
+    /// </summary>
+    public class RequestProfilerModel
+    {
+        public DateTimeOffset RequestTime { get; set; }
+
+        public HttpContext Context { get; set; }
+
+        public string Request { get; set; } = string.Empty;
+
+        public string Response { get; set; } = string.Empty;
+
+        public DateTimeOffset ResponseTime { get; set; }
+    }
+}
---
+++ src/Bet.AspNetCore.Middleware/Diagnostics/RequestResponseLoggingMiddleware.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+using Microsoft.AspNetCore.Http;
+using Microsoft.IO;
+
+namespace Bet.AspNetCore.Middleware.Diagnostics
+{
+    internal class RequestResponseLoggingMiddleware
+    {
+        private const int ReadChunkBufferLength = 4096;
+
+        private readonly RequestDelegate _next;
+        private readonly RecyclableMemoryStreamManager _recyclableMemoryStreamManager;
+        private readonly Action<RequestProfilerModel> _requestResponseHandler;
+
+        public RequestResponseLoggingMiddleware(RequestDelegate next, Action<RequestProfilerModel> requestResponseHandler)
+        {
+            _next = next;
+            _recyclableMemoryStreamManager = new RecyclableMemoryStreamManager();
+            _requestResponseHandler = requestResponseHandler;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            var model = new RequestProfilerModel
+            {
+                RequestTime = default,
+                Context = context,
+                Request = await FormatRequest(context)
+            };
+
+            var originalBody = context.Response.Body;
+
+            using (var newResponseBody = _recyclableMemoryStreamManager.GetStream())
+            {
+                context.Response.Body = newResponseBody;
+
+                await _next(context);
+
+                newResponseBody.Seek(0, SeekOrigin.Begin);
+                await newResponseBody.CopyToAsync(originalBody);
+
+                newResponseBody.Seek(0, SeekOrigin.Begin);
+                model.Response = FormatResponse(context, newResponseBody);
+                model.ResponseTime = default;
+                _requestResponseHandler(model);
+            }
+        }
+
+        public async Task<string> GetRequestBody(HttpRequest request)
+        {
+
+            request.EnableBuffering();
+
+            using (var requestStream = _recyclableMemoryStreamManager.GetStream())
+            {
+                await request.Body.CopyToAsync(requestStream);
+                request.Body.Seek(0, SeekOrigin.Begin);
+                return ReadStreamInChunks(requestStream);
+            }
+        }
+
+        private static string ReadStreamInChunks(Stream stream)
+        {
+            stream.Seek(0, SeekOrigin.Begin);
+            string result;
+            using (var textWriter = new StringWriter())
+            using (var reader = new StreamReader(stream))
+            {
+                var readChunk = new char[ReadChunkBufferLength];
+                int readChunkLength;
+
+                // do while: is useful for the last iteration in case readChunkLength < chunkLength
+                do
+                {
+                    readChunkLength = reader.ReadBlock(readChunk, 0, ReadChunkBufferLength);
+                    textWriter.Write(readChunk, 0, readChunkLength);
+                }
+                while (readChunkLength > 0);
+
+                result = textWriter.ToString();
+            }
+
+            return result;
+        }
+
+        private string FormatResponse(HttpContext context, MemoryStream newResponseBody)
+        {
+            var request = context.Request;
+            var response = context.Response;
+
+            return $"Http Response Information: {Environment.NewLine}" +
+                    $"Schema:{request.Scheme} {Environment.NewLine}" +
+                    $"Host: {request.Host} {Environment.NewLine}" +
+                    $"Path: {request.Path} {Environment.NewLine}" +
+                    $"QueryString: {request.QueryString} {Environment.NewLine}" +
+                    $"StatusCode: {response.StatusCode} {Environment.NewLine}" +
+                    $"Response Body: {ReadStreamInChunks(newResponseBody)}";
+        }
+
+        private async Task<string> FormatRequest(HttpContext context)
+        {
+            var request = context.Request;
+
+            return $"Http Request Information: {Environment.NewLine}" +
+                        $"Schema:{request.Scheme} {Environment.NewLine}" +
+                        $"Host: {request.Host} {Environment.NewLine}" +
+                        $"Path: {request.Path} {Environment.NewLine}" +
+                        $"Headers: {string.Join("; ", request.Headers.Select(x => $"{x.Key}={x.Value}").ToArray())}" +
+                        $"QueryString: {request.QueryString} {Environment.NewLine}" +
+                        $"Request Body: {await GetRequestBody(request)}";
+        }
+    }
+}
